4b110e12665196b068b66cff05c1a34af1ec1195
Emanuele Ballarin <emanuele@ballarin.cc>
2020-03-01 06:07:58 +0100

    Rationalize organization

--- src/mnist_baseline.py
+++ src/mnist_baseline.py
@@ -1,5 +1,3 @@
-# Reference implementation: https://github.com/pytorch/examples/blob/master/mnist/main.py with slight modifications.
-
 from __future__ import print_function
 import argparse
 import torch
@@ -7,28 +5,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-
-# Net architecture is preserved.
-# Weights & biases total: 431080
+from torch.optim.lr_scheduler import StepLR
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 50)
-        x = F.relu(self.fc1(x))
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -42,8 +44,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                           100. * batch_idx / len(train_loader), loss.item()))
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
 
 def test(args, model, device, test_loader):
@@ -61,26 +63,23 @@ def test(args, model, device, test_loader):
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-
-
-# Default parameters are tweaked:
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=32, metavar='N',          # As per Graphcore: https://www.graphcore.ai/posts/revisiting-small-batch-training-for-deep-neural-networks
-                        help='input batch size for training (default: 32)')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=20, metavar='N',              # Doubling the epochs (cfr. above)
-                        help='number of epochs to train (default: 20)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -88,7 +87,7 @@ def main():
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
 
-    parser.add_argument('--save-model', action='store_true', default=True,         # Acquire a baseline model
+    parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -99,25 +98,27 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('../data', train=True, download=True,
-                           transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
-            ])),
-            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
+        scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
---
+++ src/radler_util.py
@@ -0,0 +1,94 @@
+################################################################################
+#                                                                              #
+# RADLER ~ (adversarially) Robust Adversarial Distributional LEaRner           #
+#                                                                              #
+# (C) 2019-* Emanuele Ballarin <emanuele@ballarin.cc>                          #
+# (C) 2019-* AI-CPS@UniTS Laboratory (a.k.a. Bortolussi Group)                 #
+#                                                                              #
+# Distribution: MIT License                                                    #
+# (Full text: https://github.com/emaballarin/RADLER/blob/master/LICENSE)       #
+#                                                                              #
+# Eventually-updated version: https://github.com/emaballarin/RADLER            #
+#                                                                              #
+################################################################################
+
+
+# IMPORTS #
+
+import torch as th
+import collections
+
+
+# FUNCTION-ALIASES #
+
+def same(lhs, rhs):
+    return lhs == rhs
+
+
+# AUXILIARY FUNCTIONS #
+
+def dictmodel_numel(dictmodel: collections.OrderedDict):
+    elems: int = 0
+    for key in dictmodel:
+        elems += dictmodel.get(key).numel()
+    return elems
+
+
+def dictmodel_flatten(dictmodel: collections.OrderedDict, th_device="cuda"):
+
+    # Enforce correct device usage
+    if (th_device != "cpu") and (th_device != "cuda"):
+        raise Exception('model_flatten: th_device specified is neither "cpu" nor "cuda".')
+
+    flatmodel: th.Tensor = th.tensor([]).to(th_device)
+
+    for key in dictmodel:
+        to_add: th.Tensor = dictmodel.get(key).flatten().to(th_device)
+        flatmodel = th.cat((flatmodel, to_add), dim=0)
+
+    flatmodel = flatmodel.to(th_device)
+
+    # Cautionary consistency check
+    if flatmodel.size() != th.Size([dictmodel_numel(dictmodel)]):
+        print("model_flatten: Number-of-Elements mismatch between FLAT and DICT models!")
+        raise Exception('THIS EXCEPTION IS A DIAGNOSTIC FEATURE. SHOULD NEVER HAPPEN! See printed message for errors.')
+
+    return flatmodel
+
+
+def dictmodel_unflatten(flatmodel: th.Tensor, as_dictmodel: collections.OrderedDict):
+    # Consistency check
+    if flatmodel.size() != th.Size([dictmodel_numel(as_dictmodel)]):
+        raise Exception('model_unflatten: Number-of-Elements mismatch between FLAT and AS_DICT models!')
+
+    dictmodel: dict = {}
+    start: int = 0
+    stop: int = 0
+
+    for key in as_dictmodel:
+        stop += as_dictmodel.get(key).numel()
+        dictmodel.update({key: flatmodel[start:stop].view_as(as_dictmodel.get(key)).to(as_dictmodel.get(key).device)})
+        start = stop
+
+    return collections.OrderedDict(dictmodel)
+
+
+def model_weightload(dictmodel: collections.OrderedDict, instantiated_model, th_device=None, only_eval: bool = True):
+    # Instantiate model as:
+    # model = TheModelClass(*args, **kwargs)
+    # Then pass model as instantiated_model
+
+    instantiated_model.load_state_dict(dictmodel)
+
+    # Enforce correct device usage
+    if th_device is not None:
+        if (th_device != "cpu") and (th_device != "cuda"):
+            raise Exception('model_weightload: th_device specified is neither "cpu" nor "cuda" (nor None).')
+        else:
+            instantiated_model.to(th_device)
+            # Make sure to call input = input.to(device) on any input tensors that you feed to the model
+
+    if only_eval:
+        instantiated_model.eval()
+
+    return None
